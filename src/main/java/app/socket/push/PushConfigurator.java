e79b8ca22f527763722c4bc07e671986b398b81b
Paulo Gladson <paulopinheio777@gmail.com>
2017-08-25 17:05:20 -0300

    Inicio

---
+++ src/main/java/app/socket/push/PushConfigurator.java
@@ -0,0 +1,30 @@
+package app.socket.push;
+
+import javax.websocket.server.ServerEndpointConfig;
+
+/**
+ *
+ * @author PauloGladson
+ */
+public class PushConfigurator extends ServerEndpointConfig.Configurator {
+
+//    public String getNegotiatedSubprotocol(List<String> supported, List<String> requested) {
+//        // Plug your own algorithm here
+//    }
+//
+//    public List<Extension> getNegotiatedExtensions(List<Extension> installed, List<Extension> requested) {
+//        // Plug your own algorithm here
+//    }
+//
+//    public boolean checkOrigin(String originHeaderValue) {
+//        // Plug your own algorithm here
+//    }
+//
+//    public void modifyHandshake(ServerEndpointConfig sec, HandshakeRequest request, HandshakeResponse response) {
+//        // Plug your own algorithm here
+//    }
+//
+//    public <T> T getEndpointInstance(Class<T> endpointClass) throws InstantiationException {
+//        // Plug your own algorithm here
+//    }
+}
